90c01d7f6b1d5ee8e5f35bc3af13689b6a436f64
didineels <didi@neels.io>
2025-08-22 23:46:36 -0400

    Make mute bar overlay span border to border in AudioToggleButton and use mapped colors/images

--- src/components/AudioToggleButton.tsx
+++ src/components/AudioToggleButton.tsx
@@ -1,65 +1,65 @@
-import soundIcon from "../../public/sound.png";
 import { COLORS } from "../maps/colors.map";
+import { ICONS } from "../maps/icons.map";
 
 type AudioToggleButtonProps = {
   audioOn: boolean;
-  setAudioOn: (on: boolean) => void;
+  setAudioOn: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export function AudioToggleButton({ audioOn, setAudioOn }: AudioToggleButtonProps) {
+  const toggleMute = () => setAudioOn(!audioOn);
+
   return (
     <button
-      onClick={() => setAudioOn(!audioOn)}
+      onClick={toggleMute}
       style={{
-        background: COLORS.maize,
-        border: `2px solid ${COLORS.purple}`,
+        width: 32,
+        height: 32,
         borderRadius: "50%",
-        width: 40,
-        height: 40,
-        fontSize: 20,
-        cursor: "pointer",
+        border: `2px solid ${COLORS.purple}`,
+        background: COLORS.maize,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         padding: 0,
-        margin: 0,
+        cursor: "pointer",
+        position: "relative",
+        overflow: "hidden",
       }}
       aria-label={audioOn ? "Mute audio" : "Unmute audio"}
     >
-      <span style={{ position: "relative", width: 24, height: 24, display: "inline-block" }}>
-        <img
-          src={soundIcon}
-          alt={audioOn ? "Sound on" : "Sound off"}
+      <img
+        src={ICONS.sound}
+        alt={audioOn ? "Sound on" : "Sound off"}
+        width={20}
+        height={20}
+        style={{ display: "block" }}
+        draggable={false}
+      />
+      {/* Mute bar overlay */}
+      {!audioOn && (
+        <svg
+          width={32}
+          height={32}
+          viewBox="0 0 32 32"
           style={{
-            width: 24,
-            height: 24,
-            display: "block",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            pointerEvents: "none",
           }}
-        />
-        {!audioOn && (
-          <svg
-            width={40}
-            height={40}
-            viewBox="0 0 40 40"
-            style={{
-              position: "absolute",
-              left: -8,
-              top: -8,
-              pointerEvents: "none",
-            }}
-          >
-            <line
-              x1={0}
-              y1={0}
-              x2={40}
-              y2={40}
-              stroke={COLORS.purple}
-              strokeWidth={4}
-              strokeLinecap="round"
-            />
-          </svg>
-        )}
-      </span>
+        >
+          <line
+            x1={0}
+            y1={0}
+            x2={32}
+            y2={32}
+            stroke={COLORS.purple}
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
+      )}
     </button>
   );
 }
---
+++ src/maps/icons.map.ts
@@ -0,0 +1,4 @@
+export const ICONS = {
+  sound: "/sound.png", // or your actual path
+  mute: "/mute.png",   // if you have a mute icon
+};
